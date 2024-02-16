09854454ace19940fdda24abaae71d295eb33c14
simonakomarovska <simonak399@gmail.com>
2024-02-17 00:25:50 +0100

    checked

--- script.js
+++ script.js
@@ -15,6 +15,31 @@ for (let i = 0; i < accordion.length; i++) {
     })
 }
 
+// modal window functions
+const signInButton = document.getElementById("signInButton");
+const signInModal = document.getElementById("signInModal");
+const closeButton = document.getElementById("closeButton");
+
+function showSignInModal() {
+    signInModal.showModal();
+    document.body.classList.add('dialog-opened');
+    document.body.style.overflow = 'hidden'; // Disable scrolling
+}
+signInButton.addEventListener("click", showSignInModal);
+function closeSignInModal() {
+    signInModal.close();
+    document.body.classList.remove('dialog-opened');
+    document.body.style.overflow = 'auto'; // Enable scrolling
+}
+closeButton.addEventListener("click", closeSignInModal);
+
+// Close modal when "Escape" key is pressed
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape') {
+        closeSignInModal();
+    }
+});
+
 // "learn more" expandable
 function toggleAdditionalInfo() {
     const learnMoreInfo = document.getElementById("learnMoreInfo");
@@ -28,30 +53,5 @@ function toggleAdditionalInfo() {
         learnMoreInfo.style.display = "inline"; 
     }
 }
-
-
-// modal window functions
-const signInButton = document.getElementById("signInButton");
-const signInModal = document.getElementById("signInModal");
-const closeButton = document.getElementById("closeButton");
-
-function showSignInModal() {
-    signInModal.showModal();
-    document.body.classList.add('dialog-opened');
-}
-signInButton.addEventListener("click", showSignInModal);
-function closeSignInModal() {
-    signInModal.close();
-    document.body.classList.remove('dialog-opened');
-}
-closeButton.addEventListener("click", closeSignInModal);
-
-
-// Close modal when "Escape" key is pressed
-document.addEventListener('keydown', function (event) {
-    if (event.key === 'Escape') {
-        closeSignInModal();
-    }
-});
 
 
